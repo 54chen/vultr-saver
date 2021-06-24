ab8c577cab6f6279bcf94e3274e39b0e62032848
54chen <czhttp@gmail.com>
2021-06-24 18:30:36 +0800

    add new function

--- create.sh
+++ create.sh
@@ -6,9 +6,14 @@ echo "\n";
 
 RUNNING=`sh test.sh|sed -n 2p`
 
-if [ -n $RUNNING ]; then
-
+if [ -n "$RUNNING" ]; then
 	echo "Already running: $RUNNING";
+	exit;
+fi
+
+FILE_NAME=`date "+%m-%d"`;
+if [ -f "temp/$FILE_NAME" ]; then
+	echo "Already running today, delete temp/$FILE_NAME and create again"
 	exit;
 fi
 
@@ -25,3 +30,5 @@ curl "https://api.vultr.com/v2/instances" \
     "plan" : "vhf-1c-1gb",
     "snapshot_id" : "061ab18f-0660-4ee1-9d6a-4383c7496796"
   }'
+
+echo $FILE_NAME > temp/$FILE_NAME
